a6ec9fc158e9518a267848e6152a9f4c2093ba33
Lavina Tewani <lavinatewani313@gmail.com>
2023-04-05 19:14:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streammlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale , Spinach and rocket smoothie')
 sTreamlit.text('hard boiled free-range egg')
