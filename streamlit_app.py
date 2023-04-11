7f86452649458cec9a6aad579389965c24c68ee7
Lavina Tewani <lavinatewani313@gmail.com>
2023-04-11 19:08:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,15 @@ streamlit.text('🐔Hard boiled free-range egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
   
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe(my_fruit_list)
-# Let's put a pick list here so they can pick the fruit they want to include pre populating to set an example 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# import pandas
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
+# streamlit.dataframe(my_fruit_list)
+# # Let's put a pick list here so they can pick the fruit they want to include pre populating to set an example 
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+# # Display the table on the page.
+# streamlit.dataframe(my_fruit_list)
 
 # put the list of selected fruits into a variable called fruits_selected 
 # lets put a pic list here so they can pick the fruits they want to include 
