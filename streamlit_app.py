6623ddff69310306ab4de1c86b6f563cd8560eed
Lavina Tewani <lavinatewani313@gmail.com>
2023-04-11 20:57:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,22 +24,22 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_to_show)
-streamlit.header('Fruityvice Fruit Advice!')
+# streamlit.dataframe(fruits_to_show)
+# streamlit.header('Fruityvice Fruit Advice!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# import requests
+# fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+# # write your own comment -what does the next line do? 
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # write your own comment - what does this do?
+# streamlit.dataframe(fruityvice_normalized)
 
-streamlit.text(fruityvice_responce.json())
+# streamlit.text(fruityvice_responce.json())
 
 
